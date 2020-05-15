e0bf875151e1f5443e46b74902d441c3e6f5e705
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-05-15 11:48:23 -0600

    Update script.js
    Change to modern JavaScript

--- src/script.js
+++ src/script.js
@@ -1,13 +1,16 @@
-function validate() {
-    const checkboxes = document.getElementsByName("unidades");
-    for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            document.getElementsByClassName('chk-group')[0].setAttribute('style', 'border-bottom: 2px solid #92FE9D');
-            document.getElementsByClassName('btn-submit')[0].removeAttribute('disabled');
-            break;
+const checkboxes = document.getElementsByName("unidades");
+
+for (const input of checkboxes) {
+    input.addEventListener('click', () => {
+        const chk_group = document.getElementsByClassName('chk-group')[0];
+        const btn_submit = document.getElementById('btn-submit');
+
+        if (input.checked) {
+            chk_group.setAttribute('style', 'border-bottom: 2px solid #92FE9D');
+            btn_submit.removeAttribute('disabled');
         } else {
-            document.getElementsByClassName('chk-group')[0].removeAttribute('style');
-            document.getElementsByClassName('btn-submit')[0].setAttribute('disabled', '');
+            chk_group.removeAttribute('style');
+            btn_submit.setAttribute('disabled', '');
         }
-    }
+    });
 }
