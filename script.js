30bfe64664da047a8b12df2dbce408a2ead4a363
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-05-19 23:44:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //  Validar que la selección de unidades de aprendizaje
-const checkboxes = document.getElementsByName("unidades");
+const checkboxes = document.getElementsByName('unidades');
 
 for (const checkbox of checkboxes) {
     checkbox.addEventListener('click', () => {
@@ -23,52 +23,54 @@ for (const checkbox of checkboxes) {
 
 //  Ladas telefónicas
 const ladas = {
-    311: ['Tepic', 'Xalisco'],
-    319: ['Del Nayar', 'Rosamorada', 'Ruiz', 'Tuxpan'],
-    322: ['Bahia de Banderas'],
-    323: ['San Blas', 'Santiago Ixcuintla'],
-    324: ['Ahuacatlan', 'Amatlan de Cañas', 'Jala'],
-    325: ['Acaponeta', 'Huajicori'],
-    327: ['Compostela', 'San Blas', 'San Pedro Lagunillas', 'Santa Maria del Oro'],
-    329: ['Bahia de Banderas'],
-    389: ['Tecuala'],
-    347: ['La Yesca']
+    0: ['325', 'Acaponeta'],
+    1: ['324', 'Ahuacatlan'],
+    2: ['324', 'Amatlan de Cañas'],
+    3: ['329', 'Bahía de Banderas'],
+    4: ['327', 'Compostela'],
+    5: ['319', 'Del Nayar'],
+    6: ['325', 'Huajicori'],
+    7: ['324', 'Ixtlán del Río'],
+    8: ['324', 'Jala'],
+    9: ['347', 'La Yesca'],
+    10: ['319', 'Rosamorada'],
+    11: ['319', 'Ruíz'],
+    12: ['323', 'San Blas'],
+    13: ['327', 'San Pedro Lagunillas'],
+    14: ['327', 'Santa María del Oro'],
+    15: ['323', 'Santiago Ixcuintla'],
+    16: ['389', 'Tecuala'],
+    17: ['311', 'Tepic'],
+    18: ['319', 'Tuxpan'],
+    19: ['311', 'Xalisco']
 };
 
 const cargar_ladas = () => {
     let option;
+    const municipio = document.querySelector('#municipio')
+
     for (const lada in ladas) {
         option = document.createElement('option');
-        option.value = lada;
-        option.textContent = `+${lada} ${ladas[lada]}`;
-        document.querySelector('#lada').appendChild(option);
+        option.value = ladas[lada][0];
+        option.textContent = `${ladas[lada][1]}`;
+        municipio.appendChild(option);
     }
 };
 
 cargar_ladas();
 
-//  Selección de lada y cambio del Selector al Input de Télefono
-let selector = document.getElementById('lada');
-selector.addEventListener('click', () => {
-    let input_telefono = document.getElementById('telefono');
-    if (selector.value == "") {
-        selector.setAttribute('style', 'border-bottom: 2px solid #D32F2F');
-    } else {
-        input_telefono.value = selector.value;
-        selector.setAttribute('style', 'border-bottom: 2px solid #92FE9D');
-    }
-});
+//
 
-//  Validar número telefónico UmU
+//  Validar número telefónico
 let input_telefono = document.getElementById('telefono');
 
 input_telefono.addEventListener('keyup', () => {
-    let selector = document.getElementById('lada');
+    let selector = document.getElementById('municipio');
     let valor = input_telefono.value;
 
     if (selector.value == valor.substring(0, 3) && valor.length === 10 && selector.value !== '') {
-        input_telefono.setAttribute('style', 'border-bottom: 2px solid #92FE9D');
+        input_telefono.setAttribute('style', 'border-bottom: 2px solid #92FE9D');   //  Verde
     } else {
-        input_telefono.setAttribute('style', 'border-bottom: 2px solid #D32F2F');
+        input_telefono.setAttribute('style', 'border-bottom: 2px solid #D32F2F');   //  Rojo
     }
 });
